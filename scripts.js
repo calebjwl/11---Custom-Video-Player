82acdc539b14eb5ccca244fb0a165e2339eeafb2
Caleb Lee <caleb.gwl@gmail.com>
2017-07-10 21:23:30 -0500

    Scrub feature

--- scripts.js
+++ scripts.js
@@ -37,13 +37,21 @@ function handleProgress() {
   progressBar.style.flexBasis = `${percent}%`;
 }
 
+function scrub(e) {
+ const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+ video.currentTime = scrubTime;
+}
+
 // Event Listeners
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
 video.addEventListener('timeupdate', handleProgress);
+video.addEventListener('click', handleProgress);
 
 toggle.addEventListener('click', togglePlay);
 skipButtons.forEach(button => button.addEventListener('click', skip));
 ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
 ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+
+progress.addEventListener('click', scrub);
